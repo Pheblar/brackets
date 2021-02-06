835f7cb8d067732003e39e83fc32cd34ba7621d9
Roman Shor <kstarfol@mail.ru>
2021-02-06 10:45:11 +0300

    init: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-    
+    let stack = [];
+    let opened = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    };
+    let closed = {
+        '}': true,
+        ']': true,
+        ')': true
+    }
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (opened[char]) {
+            stack.push(char);
+        } else if (closed[char]) {
+            if (opened[stack.pop()] !== char) return false;
+        }
+    }
+    return stack.length === 0;
 }
